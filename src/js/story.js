f62facf80efe47506327ed9d6f59c61e74e1cb86
melkoff <vasamelko@gmail.com>
2024-03-17 15:44:00 +0200

    del swiper slider, add custom slider

--- src/js/story.js
+++ src/js/story.js
@@ -1,5 +1,36 @@
-var swiper = new Swiper(".storySlider", {
-   slidesPerView: 4,
-   spaceBetween: 30,
-   freeMode: true,
+document.addEventListener('DOMContentLoaded', function () {
+   const storyGallery = document.querySelector('.story-gallery');
+   let isDragging = false;
+   let startX = null;
+   let scrollLeft = 0;
+
+   function handleStart(e) {
+      isDragging = true;
+      startX = e.type === 'touchstart' ? e.touches[0].pageX - storyGallery.offsetLeft : e.pageX - storyGallery.offsetLeft;
+      scrollLeft = storyGallery.scrollLeft;
+   }
+
+   function handleMove(e) {
+      if (!isDragging) return;
+      e.preventDefault();
+      const x = e.type === 'touchmove' ? e.touches[0].pageX - storyGallery.offsetLeft : e.pageX - storyGallery.offsetLeft;
+      const walk = (x - startX) * 3; // Збільшуємо швидкість прокрутки
+      storyGallery.scrollTo({
+         left: scrollLeft - walk,
+         behavior: 'smooth' // Додаємо плавність
+      });
+   }
+
+   function handleEnd() {
+      isDragging = false;
+   }
+
+   storyGallery.addEventListener('mousedown', handleStart);
+   storyGallery.addEventListener('mousemove', handleMove);
+   storyGallery.addEventListener('mouseup', handleEnd);
+
+   storyGallery.addEventListener('touchstart', handleStart);
+   storyGallery.addEventListener('touchmove', handleMove);
+   storyGallery.addEventListener('touchend', handleEnd);
 });
+
