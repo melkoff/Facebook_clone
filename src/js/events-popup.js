f798c1b436f55972652e16b29787508af649592e
melkoff <vasamelko@gmail.com>
2024-03-27 14:26:15 +0200

    Update events-popup.js

--- src/js/events-popup.js
+++ src/js/events-popup.js
@@ -1,5 +1,4 @@
 document.getElementById("showAllEventsPopup").addEventListener("click", function () {
-
    document.querySelector(".events-popup").classList.add("active-events");
    console.log("show events popup");
 })
@@ -12,10 +11,7 @@ const closeSingleEvent = document.querySelectorAll(".del-event");
 const getSingleEvent = document.querySelectorAll(".my-event");
 
 for (let i = 0; i < closeSingleEvent.length; i++) {
-
    closeSingleEvent[i].onclick = function () {
-      let getSingleEvent = this.parentElement;
-      getSingleEvent.style.display = "none";
-
+      getSingleEvent[i].style.display = "none";
    }
 }
